be5a20719174e5eba596c7a7bbb6c8335fb1f1a7
sandae <hzyeti@gmail.com>
2018-09-05 14:02:50 +0800

    Update epubfreq.sh

--- epubfreq.sh
+++ epubfreq.sh
@@ -1,57 +1,68 @@
 #!/usr/bin/sh
-#set -x
-
+set -x
+#````````````````
 ########### 全 局 变 量################## 
 #-----------------------------------
 #词典文件格式为：\tWORD\tMEANING
-SOURCEDICT=dict
-SOURCELIST=dict_wordlist
+SOURCEDICT=coca3w9dict
+SOURCELIST=coca3w9list
 
 
 #生成Word列表
-if [  ! -f dict_wordlist ];then
+if [  ! -f coca3w9list ];then
    cat $(eval echo "$SOURCEDICT") \
    | head -n 38000 \
-   | awk '{FS="\t"}{print $2}' > dict_wordlist
+   | awk '{FS="\t"}{print $2}' > coca3w9list
 fi
 #-------------------------------------
-
-#脚本参数（如未使用参数则转换当前目录所有epub文件）
-EPUB_FILES=$1
-if  [ ! -n "$1" ]; then
- EPUB_FILES=`ls *.epub`
-fi
-
 WORKSPACE=epub
 DATE=`date "+%d%H%M"`
 echo $DICT_COUNT
 rm -rf epub
-rm  *.zip
+rm -f *.zip
 mkdir -p $WORKSPACE
+
+#````````````````
+
+if  [ ! -n "$1" ]; then
+
+rename 's/ /@/g' *.epub
+EPUB_FILES=*.epub
+echo $EPUB_FILES
+
+else
+EPUB_FILES=`echo $1 | sed 's/ /@/g'`
+mv "$1" "$EPUB_FILES"
+fi
+
 
 
 #词频等级数量（实际数量为NO-1） 
 NO=6
 #Word分频等级界限 LEVEL
-
+#Junior
+echo "collins每个等级分别为1342/1388/1831/3400/8228/20581(无星)"
 e1=1 e2=2000 e3=4000 e4=6500 e5=10000 e6=18000 e7=26000
+#e1=20011 e2=2500 e3=3000 e4=3500 e5=4000 e6=4500
+#e1=2000 e2=4500 e3=6500 e4=9000 e5=12000 e6=15000
+#Middle
 i1=4500 i2=6500 i3=10000 i4=15000 i5=20000 i6=28000
+#Senior
 a1=6000 a2=9000 a3=15000 a4=20000 a5=40000 a6=50000
 #--------------------------------------------
 #词频显示样式MODEL
-# 1 角标 Subscript (1为最高频)
+# （1/2） 角标 Subscript (1为最高频)
 b1='\1¹' b2='\1²' b3='\1³' b4='\1⁴' b5='\1⁵' b6='\1⁶'
 
-# 2 彩色
+# （2/2） 彩色
 #暗色
-d1='<a style="color:DimGray; text-decoration:none;"\1\2'
+
+d1='<a style="color:Gray; text-decoration:none;"\1\2'
 d2='<a style="color:green; text-decoration:none;"\1\2'
 d3='<a style="color:teal; text-decoration:none;"\1\2'
 d4='<a style="color:olive; text-decoration:none;"\1\2'
-d5='<a style="color:navy; text-decoration:none;"\1\2'
+d5='<a style="color:maroon; text-decoration:none;"\1\2'
 d6='<a style="color:maroon; text-decoration:none;"\1\2'
-
-
 
 #亮色
 #d1='<a style="color:LimeGreen; text-decoration:none;"\1'
@@ -61,6 +72,12 @@ d6='<a style="color:maroon; text-decoration:none;"\1\2'
 #d5='<a style="color:fuchsia; text-decoration:none;"\1'
 #d6='<a style="color:lime; text-decoration:none;"\1'
 
+#d1='<a style="color:navy; text-decoration:none;"\1'
+#d2='<a style="color:maroon; text-decoration:none;"\1'
+#d3='<a style="color:green; text-decoration:none;"\1'
+#d4='<a style="color:orange; text-decoration:none;"\1'
+#d5='<a style="color:purple; text-decoration:none;"\1'
+#d6='<a style="color:lime; text-decoration:none;"\1'
 
 c1='<font color=DimGray>\1</font>'
 c2='<font color=green>\1</font>'
@@ -68,6 +85,8 @@ c3='<font color=teal>\1</font>'
 c4='<font color=olive>\1</font>'
 c5='<font color=purple>\1</font>'
 c6='<font color=maroon>\1</font>'
+
+#暗深红色	crimson
 
 # 亮色 blue red fuchsia lime aqua yellow
 # 暗色 navy maroon green teal  purple
@@ -82,7 +101,42 @@ c6='<font color=maroon>\1</font>'
 pre(){
 ##预处理epub文件######################
 
-rename 's/[ ]+/_/g' $EPUB_FILES #删除文件名中的空格
+####脚本参数，有参数（书名）时转换该书，否则转换所有epub
+###EPUB_FILES=$1
+###if  [ ! -n "$1" ]; then
+### echo "IS NULL"
+### EPUB_FILES=`*.epub`
+#### echo "$EPUB_FILES"
+
+###   else
+###  echo "NOT NULL"
+###     EPUB_FILES=$1
+###     echo "$EPUB_FILES"
+###fi
+### echo "$EPUB_FILES"
+###exit 0
+
+##rename 's/ /@/g' *.epub #删除文件名中的空格
+###修改epub文件后缀为zip并解压到WORKSPACE目录下
+##for F in *.epub
+##do
+##   cp $F ${F%.epub}.zip
+
+
+##done
+###批量解压缩到WORKSPACE目录下
+##    for ZIP in *.zip
+##    do
+##        FILENAME=$(echo $ZIP|cut -d'.' -f1)
+##        unzip -q $ZIP -d $WORKSPACE/$FILENAME
+##    done
+
+
+#$$$%%%^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+
+#rename 's/[ ]+/_/g' $1 #删除文件名中的空格
 #修改epub文件后缀为zip并解压到WORKSPACE目录下
 for F in $EPUB_FILES
 do
@@ -95,8 +149,11 @@ done
     for ZIP in *.zip
     do
         FILENAME=$(echo $ZIP|cut -d'.' -f1)
-        unzip -q $ZIP -d $WORKSPACE/$FILENAME
+        unzip $ZIP -d $WORKSPACE/$FILENAME
     done
+
+
+#&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
 #---------------------------------------------
 #函数 提取所有要处理的html或xhtml文件
@@ -104,6 +161,7 @@ done
 #所有*html文件，包括小文件
 html(){ 
 for html in $(find epub -maxdepth 10 -type f -name "*.*html");do
+#for i in 'epub/*/*/*.*html' 'epub/*/*.*html' ;do
     echo "$html"
 done
 }
@@ -114,7 +172,8 @@ HTML=$(html)
 #查找html/xhtml文件
 for COUNT in $(find epub -maxdepth 10 -type f -name "*.*ml")
 do
-#删除html<标签>部分内容
+#    H=$(ls -l $HTML | awk '{print $5}')
+#删除<尖括号>部分内容
     H=$(sed -e "s/>/>\n/g" -e "s/</\n</g" $COUNT \
 	| sed "/</d" | wc -w) 
 
@@ -146,6 +205,7 @@ prefilter(){
 #每次转换从html文件提取代码文字（需剔除）
 #尖括号换行|word换行|删非文字|删数字|删空行|去重
 #列表单词收尾加@标记（@word@）
+#echo $HTML
 FILTER=`sed -r -e "s/>/>\n/g" -e "s/</\n</g" $HTMLBIG \
       | sed -n "/</p" \
       | sed -e "s/\b/\n/g" -e "s/_/\n/g" \
@@ -172,10 +232,19 @@ echo "开始剔除........"
 
 ####根据以上生成的剔除列表从词典word列表中删除
 #给词典word列表加@标记@ | 剔除FILTRATE中的word
+#记录：echo或cat等打印方式是否换行，会影响sed的执行
+#     如果不换行，sed会当作一行来处理（废话），换行的话正常，
+#     即使sed在执行中还是是当成一行来处理的。 对文件如此，对传递的变量呢？
+#     对变量来说，需要“正确执行”，如cat $(eval echo "$SOURCELIST") 
+#     相当与直接cat 文件
+#     2、var=$() 和var=''测试效果相同，区别未知
+
 WORDLIST=`cat $(eval echo "$SOURCELIST") \
         | sed -e 's/^/@/g' -e 's/$/@/g' \
         | sed -r "s#($FILTRATE)#\1AAAA#g" \
         | sed 's/@//g' | sed '/AAAA/d'`
+
+#echo "$WORDLIST"
 
 ###根据以上生成的剔除列表从词典文件中删除相应条目
 #词典添加\t@\t标记 | 把FILTRATE从词典中剔除 | 删除标记
@@ -183,7 +252,7 @@ FOOTDICT=`cat $(eval echo "$SOURCEDICT") \
         | sed -r 's/\t/@\t@/g' \
         | sed -r "s#($FILTRATE)#\1\tAAAA#g" \
         | sed '/AAAA/d' | sed "/^$/d"`
-echo "$FOOTDICT" > FOOTDICT
+#echo "$FOOTDICT" > FOOTDICT
 echo "prefilter预过滤完成"
 echo "开始转换..........."
 }
@@ -207,6 +276,7 @@ mkdir -p Result && cd $WORKSPACE
 for i in `find ./* -type d`;do zip -q $i.zip `find $i \
 | grep -v "$i$"`;done
 rename 's/%#/ /g' *.zip 
+rename 's/@/ /g' *.zip
 
 #添加识别前缀
 for F in *.zip ; do 
@@ -214,7 +284,10 @@ for F in *.zip ; do
 	| tr 'a-z' 'A-Z')$DATE-${F%.zip}.epub"
 done
 
-cd ../ && rename 's/%#/ /g' *.epub && rm *.zip 
+cd ../
+rename 's/%#/ /g' *.epub
+rename 's/@/ /g' *.epub
+rm -f *.zip 
 
 echo "##############################################"
 echo "#                 转换完成!                   #"
@@ -234,6 +307,9 @@ LEVEL=$1 MODEL=$2 ADDNOTE=$3
 
 #-----------------------------------------------------------------
 #根据选定的Level等级提取相应等级的word范围
+    #$NO为相应模式的等级数量，此处生成循环,echo (6..1) > 6 5 4 3 2 1
+
+    # \$$LEVEL$[ n+1 ]获取顶部相应Level的分级，NO=6则该函数值为e7
     for n in $( eval echo {$(($NO-1))..1});do
     #LISTN为相应等级的word列表 head -n j6 | tail -n j5，以此类推
     LISTN=$(echo "$WORDLIST" \
@@ -250,8 +326,11 @@ LEVEL=$1 MODEL=$2 ADDNOTE=$3
       | tr '\n' '|' \
       | sed 's/|$//'`
 
-#根据ListN和词频样式Model逐级为html文件添加标记
+#根据ListN和词频样式Model逐级为html文件添加标记，循环结束
+#i忽略大小写
+
 sed -r -i "s#($EXP)([\,\.\"]+)*#$(eval echo \$$MODEL$n)#gi" $HTMLBIG
+#sed -r -i "s#($EXP)#$(eval echo \$$MODEL$n)#g" $HTMLBIG
 
     done
 echo "添加样式标记完成"
@@ -282,18 +361,29 @@ fi
     | sed 's/@//g' > dictlist
     python -c 'import dic9;print dic9.DReplace("dictlist","'$WORKSPACE'")'
 
+#解决python无法保留替换对象小写的问题：sed区分大小写，大写不替换
+#此处匹配python替换过sed未替换的word，将首字母恢复大写，并删除多余标签代码
+
+#    sed -ri 's/(\w+)<a epub:type="noteref" href="#footnote_[0-9]*"><\/a>/\u\1/g' $HTMLBIG
 
 }
 
 #未成功添加词频样式只加入释义的情况下删除脚注代码并回复首字母大写-临时解决办法
 clear_left(){
     sed -ri -e 's/([^"]) epub.*?\>(\w+)<\/a>/\1 \u\2/g' $HTMLBIG
+#    sed -ri -e 's/([^:])" epub.*?\>(\w+)<\/a>/\1 \u\2/g' $HTMLBIG
 sed -ri -e 's/([^"])\s+epub:type="noteref" href="#footnote_[0-9]+">(\w+)<\/a>/\1 \u\2/g' $HTMLBIG
+#sed -ri -e 's/([^:])"\s+epub:type="noteref" href="#footnote_[0-9]+">(\w+)<\/a>/\1 \u\2/g' $HTMLBIG
 sed -ri -e 's/[^"] epub:type="noteref" href="#footnote_[0-9]*">(\w+ \w+)<\/a>/ \u\1/g' $HTMLBIG
 sed -ri -e 's/[^"] epub:type="noteref" href="#footnote_[0-9]*">(\w+ \w+ \w+)<\/a>/ \u\1/g' $HTMLBIG
 }
 
-
+clean(){
+##未成功添加词频样式只加入释义的情况下删除脚注代码-临时解决办法
+sed -ri 's/[^"] epub:type="noteref" href="footnote_[0-9]*">(\w+\s*)<\/a>/ \1/g' $HTMLBIG
+sed -ri 's/[^"] epub:type="noteref" href="#footnote_[0-9]*">(\w+)<\/a>/ \1/g' $HTMLBIG
+sed -ri 's/<a style="color:\w+; text-decoration:none;"([^ ])([^e])/\1\2/g' $HTMLBIG
+}
 #选择Model b且添加footnote时，最后转换 ⁿN 标记为[⁰¹²³⁴⁵⁶⁷⁸⁹]
 rep_sub(){
     sed -i "s/ⁿ<\/a>\([⁰¹²³⁴⁵⁶⁷⁸⁹]\+\)/\1<\/a>/g" $HTMLBIG
